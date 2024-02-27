ae3e73bbe51bf26fce4af822fe0f9939703fa590
ericlivshiz <111153545+ericlivshiz@users.noreply.github.com>
2024-02-26 17:13:56 -0800

    Update Program.h

--- src/Program.h
+++ src/Program.h
@@ -26,6 +26,8 @@ private:
 	unsigned SCR_WIDTH = 1920;
 	unsigned SCR_HEIGHT = 1080;
 
+	// This is initialized here so the program can run
+	// but it will belong to the game code in general
 	const float PLAYER_SPEED = 50.0f;
 	const float PLAYER_SHIFT_SPEED = 17.0f;
 	const float PLAYER_CROUCH_SPEED = 10.0f;
